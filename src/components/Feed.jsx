30bfa722c5117039223db69e9f1cc136ac287c7f
Adrian Lopez <adrian.08lv@gmail.com>
2022-09-23 18:40:16 -0300

    primer config de sidebar, primeros estilos de typography

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,8 +5,12 @@ const Feed = () => {
   return (
     <Stack sx={{ 
       flexDirection: { sx:"column", md:"row" } }}>
-      <Box sx={{ height: { sx: 'auto',  md: '92vh' } }}>
-      
+      <Box sx={{ height: { sx: 'auto',  md: '92vh' }, borderRight: '1px solid #3d3d3d', 
+      px:{ sx: 0, md: 2}}}>
+      Sidebar
+      <Typography className='copyright' variant="body2" sx={{ mt: 1.5, color: '#fff'}}>
+        Copyright 2022 | Not Youtube by Adrian Lopez
+      </Typography>
       </Box>
     </Stack>
   )
