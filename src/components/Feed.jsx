55f0bd9217fb3b6950303a8053952d5aea9087d2
Adrian Lopez <adrian.08lv@gmail.com>
2022-09-29 20:20:58 -0300

    La app funciona, ya trae los videos de la API y los muestra segun cada categoria, sin terminar aun

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,9 +6,11 @@ import { fetchFromAPI } from '../utils/fetchFromAPI'
 const Feed = () => {
 
   const [selectedCategory, setSelectedCategory] = useState('New')
+  const [videos, setVideos] = useState([])
 
   useEffect(() => {
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
   }, [selectedCategory])
 
   return (
@@ -29,10 +31,9 @@ const Feed = () => {
         mb={2} sx={{ color: 'white' }}>
           {selectedCategory} <span style={{ color: '#ff0000'}}>videos</span>
         </Typography>
-
-        <Videos videos={[]}/>
+        <Videos videos={videos}/>
       </Box>
-    </Stack>
+    </Stack>  
   )
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Stack, Box } from '@mui/material'
+import { VideoCard } from './'
 
-const Videos = () => {
+const Videos = ({ videos }) => {
   return (
-    <div>Videos</div>
+      <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+        {videos.map((item, idx) => (
+          <Box key={idx}>
+            {item.id.videoId && <VideoCard video={item} />}
+          </Box>
+        ))}
+      </Stack>
   )
 }
 
