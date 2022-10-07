bb9075be2705241c733f89970cb6ba228593c80e
Adrian Lopez <adrian.08lv@gmail.com>
2022-10-07 20:50:41 -0300

    Terminado perfil de canales, empezando searchFeed, actualmente no compila pues en proceso

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/material'
-import { Videos } from './'  
+import { Videos, ChannelCard } from './'  
 import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const ChannelDetail = () => {
@@ -25,8 +25,12 @@ const ChannelDetail = () => {
           background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,81,1) 32%, rgba(0,189,255,1) 100%)',
           zIndex: 10,
           height:'300px'
-      }}
-      />
+      }}/>
+        <ChannelCard channelDetail={channelDetail}/>
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: '100px' } }}/>
+          <Videos videos={videos} />
       </Box>
     </Box>
   )
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, Typography } from '@mui/material'
+import { Videos } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${}`)
+    .then((data) => setVideos(data.items))
+  }, [])
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight="bold" 
+        mb={2} sx={{ color: 'white' }}>
+          {selectedCategory} <span style={{ color: '#ff0000'}}>videos</span>
+        </Typography>
+
+        <Videos videos={videos}/>
+    </Box> 
   )
 }
 
